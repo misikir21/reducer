68487aa597bef1e9148b1c2e806ed2585c270504
misikira <misikirteka@gmail.com>
2023-09-23 18:17:49 +0300

    add use reducer to my projce usig fake api which is just oly hosted o me local machie

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import './index.css';
 import Header from './Header';
 import Main from './Main';
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 function App() {
+  const initalState={
+    questions:[],
+
+    status:'loading'
+  }
+
+
+
+  function reducer(state,action){
+    switch(action.type){
+      case"datarecived":
+      return{
+        ...state,
+        questions:action.payload,
+        status:"ready"
+      }
+      case"datafailed":
+      return{
+        ...state,
+        status:"error"
+      }
+     default:
+      throw new Error("ACTOION UNKOWN") 
+    }
+  }
+
+
+
+
+ const [state,dispatch]=useReducer(reducer,initalState)
+
+
   useEffect(function (){
     fetch("http://localhost:8000/questions")
     .then((res)=>res.json())
-    .then((data)=>console.log(data))
-    .catch((err)=>console.log9("Error"))
+    .then((data)=>dispatch({type:'datarecived',payload:data}))
+    .catch((err)=>dispatch({type:'datafailed'}))
   },[])
   return (
     <div className='app'>

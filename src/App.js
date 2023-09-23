94d2152eefc1769fcd436911e3648653acb67418
misikira <misikirteka@gmail.com>
2023-09-23 11:28:10 +0300

    add json file to use like as data api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './index.css';
 import Header from './Header';
-
+import Main from './Main';
+import { useEffect } from 'react';
 function App() {
+  useEffect(function (){
+    fetch("http://localhost:8000/questions")
+    .then((res)=>res.json())
+    .then((data)=>console.log(data))
+    .catch((err)=>console.log9("Error"))
+  },[])
   return (
     <div className='app'>
       <Header />
+      <Main>
+        <p>15</p>
+        <p>Qestions</p>
+
+      </Main>
     </div>
   );
 }

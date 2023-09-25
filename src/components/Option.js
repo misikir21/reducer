2305bde244b1bbb65fd74399f988d816d65578c5
misikira <misikirteka@gmail.com>
2023-09-25 10:38:33 +0300

    marking correct and wrong answer

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export default function Option({question}) {
-  return (
-    <div className='option'>
-            {question.options.map((option)=> (<button className='btn btn-option' key={option}>{option}</button>))
-        }        </div>
-  )
+export default function Option({ question, dispatch, answer }) {
+	return (
+		<div className="option">
+			{question.options.map((option, index) => (
+				<button
+					className={`btn btn-option ${index === answer ? "answer" : ""} ${
+						index.correctOption ? "correct" : "wrong"
+					}`}
+					key={option}
+					onClick={() => dispatch({ type: "newanswer", payload: index })}
+				>
+					{option}
+				</button>
+			))}{" "}
+		</div>
+	);
 }

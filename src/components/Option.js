ee21eead97de56eb98e4205f8a9116e50fb3d01b
misikira <misikirteka@gmail.com>
2023-09-25 10:48:59 +0300

    disable all other buttons once the answer key is pressed

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 export default function Option({ question, dispatch, answer }) {
+	const hasanswered = answer !== null;
 	return (
 		<div className="option">
 			{question.options.map((option, index) => (
 				<button
 					className={`btn btn-option ${index === answer ? "answer" : ""} ${
-						index.correctOption ? "correct" : "wrong"
+						hasanswered
+							? index === question.correctOption
+								? "correct"
+								: "wrong"
+							: ""
 					}`}
 					key={option}
 					onClick={() => dispatch({ type: "newanswer", payload: index })}
+					disabled={hasanswered}
 				>
 					{option}
 				</button>
